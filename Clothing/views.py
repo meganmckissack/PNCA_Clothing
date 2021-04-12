bd390f40d0b4c2edfa548860be123dc979bbb39f
Emma Harris <emmaharris@Emmas-MacBook-Air.local>
2021-04-12 13:35:11 -0700

    first commit

--- Clothing/views.py
+++ Clothing/views.py
@@ -3,11 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "Clothing/index.html", {
-        "links": links
-    })
-    
-def add(request):
-    return render(request, "Clothing/add.html", {
-        "links": links
-    })
+    return render(request, "Clothing/index.html")
